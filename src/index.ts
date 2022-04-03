600a04669d254a63e80250dc48b3c1cdc4224670
meh0001 <nolan.closky@gmail.com>
2022-04-03 13:39:28 -0600

    yes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import { SapphireClient } from '@sapphire/framework';
-import config from './config';
 import dotenv from 'dotenv';
 dotenv.config()
 
 const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
 
-client.login(token);
+client.login(process.env.DISCORD_TOKEN);
