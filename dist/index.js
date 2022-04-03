b3b2a68c63d1c38da9d2d996a0422009c8a487bb
meh0001 <nolan.closky@gmail.com>
2022-04-03 13:57:13 -0600

    more

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const framework_1 = require("@sapphire/framework");
-require("./config");
 const dotenv_1 = tslib_1.__importDefault(require("dotenv"));
 dotenv_1.default.config();
 const client = new framework_1.SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
