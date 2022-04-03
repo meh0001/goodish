51f51bd42cefd14de3cfcf87de367db6d5319ec8
meh0001 <nolan.closky@gmail.com>
2022-04-03 14:31:21 -0600

    pls work

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,23 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 const client = new SapphireClient({
-    defaultPrefix: '!',
-    caseInsensitiveCommands: true,
-    intents: ['GUILDS', 'GUILD_MESSAGES'] });
+	defaultPrefix: '!',
+	regexPrefix: /^(hey +)?bot[,! ]/i,
+	caseInsensitiveCommands: true,
+	shards: 'auto',
+	intents: [
+		'GUILDS',
+		'GUILD_MEMBERS',
+		'GUILD_BANS',
+		'GUILD_EMOJIS_AND_STICKERS',
+		'GUILD_VOICE_STATES',
+		'GUILD_MESSAGES',
+		'GUILD_MESSAGE_REACTIONS',
+		'DIRECT_MESSAGES',
+		'DIRECT_MESSAGE_REACTIONS'
+	]
+});
+
+    
 
 client.login(process.env.DISCORD_TOKEN);
--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,9 +1,56 @@
-import { Listener } from '@sapphire/framework';
-import type { Client } from 'discord.js';
+import type { ListenerOptions, PieceContext } from '@sapphire/framework';
+import { Listener, Store } from '@sapphire/framework';
+import { blue, gray, green, magenta, magentaBright, white, yellow } from 'colorette';
 
-export class ReadyListener extends Listener {
-  public run(client: Client) {
-    const { username, id } = client.user!;
-    this.container.logger.info(`Successfully logged in as ${username} (${id})`);
-  }
+const dev = process.env.NODE_ENV !== 'production';
+
+export class UserEvent extends Listener {
+	private readonly style = dev ? yellow : blue;
+
+	public constructor(context: PieceContext, options?: ListenerOptions) {
+		super(context, {
+			...options,
+			once: true
+		});
+	}
+
+	public run() {
+		this.printBanner();
+		this.printStoreDebugInformation();
+	}
+
+	private printBanner() {
+		const success = green('+');
+
+		const llc = dev ? magentaBright : white;
+		const blc = dev ? magenta : blue;
+
+		const line01 = llc('');
+		const line02 = llc('');
+		const line03 = llc('');
+
+		// Offset Pad
+		const pad = ' '.repeat(7);
+
+		console.log(
+			String.raw`
+${line01} ${pad}${blc('1.0.0')}
+${line02} ${pad}[${success}] Gateway
+${line03}${dev ? ` ${pad}${blc('<')}${llc('/')}${blc('>')} ${llc('DEVELOPMENT MODE')}` : ''}
+		`.trim()
+		);
+	}
+
+	private printStoreDebugInformation() {
+		const { client, logger } = this.container;
+		const stores = [...client.stores.values()];
+		const last = stores.pop()!;
+
+		for (const store of stores) logger.info(this.styleStore(store, false));
+		logger.info(this.styleStore(last, true));
+	}
+
+	private styleStore(store: Store<any>, last: boolean) {
+		return gray(`${last ? '└─' : '├─'} Loaded ${this.style(store.size.toString().padEnd(3, ' '))} ${store.name}.`);
+	}
 }
